39b40633f74369c7370cd7ca1b9eacac33953c43
Juan N <cristhiann017@gmail.com>
2023-09-21 15:00:02 -0500

    funcional

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+
 
 class User extends Authenticatable
 {
@@ -17,6 +21,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -58,4 +63,17 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+    public function posts(){
+        return $this->HasMany(Post::class);
+    }
+    public function adminlte_desc()
+        {
+            return Auth::user()->email;
+
+        }
+
+        public function adminlte_profile_url()
+        {
+            return 'profile.show';
+        }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <div class="container py-8">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
        @livewire('posts-index')
     </div>
 </x-app-layout>
