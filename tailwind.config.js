a8f0c1abc880e3ffe5026ff3f448d425dd3d4a6f
Juan N <cristhiann017@gmail.com>
2023-09-20 21:50:11 -0500

    wireui incluido

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,8 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './vendor/wireui/wireui/resources/**/*.blade.php',
-        './vendor/wireui/wireui/ts/**/*.ts',
-        './vendor/wireui/wireui/src/View/**/*.php'
     ],
-    presets: [
-        require('./vendor/wireui/wireui/tailwind.config.js')
-    ],
+
     theme: {
         extend: {
             fontFamily: {
@@ -27,6 +22,5 @@ export default {
     plugins: [forms, typography],
     corePlugins:{
         container:false,
-    },
-   
+    }
 };
